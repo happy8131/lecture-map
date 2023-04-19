e551d36a54d45269f178e4f3844a6993eb36c23d
happy8131 <lion8131@naver.com>
2023-04-19 23:06:09 +0900

    get Server Side Props, CSR

--- pages/section1/links.tsx
+++ pages/section1/links.tsx
@@ -1,10 +1,24 @@
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 export default function Links() {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.prefetch('/section1/getStaticProps');
+  }, [router]);
   return (
     <main>
       <h1>Links</h1>
-      <Link href="/section1/getStaticProps">/getStaticProps</Link>
+      <button
+        onClick={() => {
+          router.push('/section1/getStaticProps');
+        }}
+      >
+        /getStatciProps
+      </button>
+      {/* <Link href="/section1/getStaticProps">/getStaticProps</Link> */}
     </main>
   );
 }
